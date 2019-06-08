c5d36cfc82c4c54bca106a8e9c702fa57ddffab0
SUDI DAVID <naohdvd1990@gmail.com>
2019-06-08 11:43:36 +0200

    This is my first commit

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            { 
+              new Employee(){Id = 1, Name = "Sudi", Department = Dept.IT, Email = "sudidav@thinktech.cd" } ,
+              new Employee(){Id = 2, Name = "Sarah", Department = Dept.HR, Email = "sarahmay@thinktech.cd" } ,
+              new Employee(){Id = 3, Name = "Helen", Department = Dept.Payroll, Email = "helenbeauty@thinktech.cd" }
+            };
+        }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e => e.Id) + 1;
+            _employeeList.Add(employee);
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
